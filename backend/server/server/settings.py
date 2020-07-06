3dd7e04632cd48706bd40457670e3ab90d0f9601
Gabriel Crowell <gabrielcrowell@Gabriels-MacBook-Pro.local>
2020-07-06 18:37:03 -0500

    wrapped up project

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c^#7k*d+ovbjc=104w$m^2^hc2-s4l&xufabbk^br1$^2x6zcp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -120,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
